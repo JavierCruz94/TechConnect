c649333d07d8f3dc86b7f90fd59131a78db287e7
JavierCruz94 <javiercruz_94@hotmail.com>
2016-11-05 17:44:31 -0600

    First Todo
    -

---
+++ PrepaNet/Assets/Scripts/PanelPreguntas1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelPreguntas1 : MonoBehaviour {
+
+	public GameObject panel;
+	public GameObject respuestas;
+
+	public void QuitarPanel() {
+		panel.SetActive (false);
+		respuestas.GetComponent<EligeRespuesta> ().contResp = 0;
+	}
+}
---
+++ PrepaNet/Assets/Scripts/RelacionaColumnas.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RelacionaColumnas : MonoBehaviour {
+	
+	public Text[] arrResp = new Text[6];
+	public GameObject panel;
+
+	//Se hicieron públicos porque BotonPanel los reinicializa a 0
+	public int contIzq = 0;
+	public int contDer = 0;
+
+	//Inicializar arreglos
+	string[,] colIzq = new string[,] { 
+		{ "", "" }, { "", "" }, { "", "" }
+	};
+	string[,] colDer = new string[,] { 
+		{ "", "" }, { "", "" }, { "", "" }
+	};
+
+	public void Start() {
+		int slotIzq = (int)Random.Range (0.0f, 2.0f);
+		int slotDer = (int)Random.Range (0.0f, 2.0f);
+		int preguntaBuena = (int)Random.Range (0.0f, 2.0f);
+		//Asignar pregunta a un slot
+		colIzq [slotIzq, 0] = BancoPreguntas.buenasColumnas [preguntaBuena, 0];
+		colIzq [slotIzq, 1] = "si";
+		colDer [slotDer, 0] = BancoPreguntas.buenasColumnas [preguntaBuena, 1];
+		colDer [slotDer, 1] = "si";
+
+		int preguntaMala1 = (int)Random.Range (0.0f, 5.0f);
+		int preguntaMala2 = (int)Random.Range (0.0f, 5.0f);
+		//Asegurarse que no sea la misma pregunta
+		while (preguntaMala1 == preguntaMala2) {
+			preguntaMala2 = (int)Random.Range (0.0f, 5.0f);
+		}
+
+		//Asignar primer pregunta mala al izq
+		AsignarPreguntas(colIzq, BancoPreguntas.malasColumnas [preguntaMala1, 0]);
+		//Asignar segunda pregunta mala al izq
+		AsignarPreguntas(colIzq, BancoPreguntas.malasColumnas [preguntaMala2, 0]);
+		//Asignar primer pregunta mala al der
+		AsignarPreguntas (colDer, BancoPreguntas.malasColumnas [preguntaMala1, 1]);
+		//Asignar segunda pregunta mala al der
+		AsignarPreguntas (colDer, BancoPreguntas.malasColumnas [preguntaMala2, 1]);
+
+		//Asignar el texto correspondiente
+		for (int j = 0; j < 3; j++) {
+			arrResp [j].GetComponent<Text> ().text = colIzq [j, 0];
+		}
+
+		for (int j = 3; j < 6; j++) {
+			arrResp [j].GetComponent<Text> ().text = colDer [j - 3, 0];
+		}
+	}
+
+	void AsignarPreguntas (string[,] arreglo, string preg) {
+		int i = 0;
+		bool salir = false;
+		while (i < 3 && !salir) {
+			if (arreglo[i, 0] == "") {
+				arreglo [i, 0] = preg;
+				arreglo [i, 1] = "no";
+				salir = true;
+			}
+			i++;
+		}
+	}
+
+	public void Validar() {
+		ValidarColumnaIzquierda ();
+		ValidarColumnaDerecha ();
+
+		if (panel.activeSelf == false) {
+
+			string respIzq = "";
+			string respDer = "";
+			int i = 0;
+			while (i < 3) {
+				if (arrResp [i].GetComponent<SeleccionaRespuesta> ().clickState)
+					respIzq = colIzq [i, 1];
+					i++;
+			}
+			i = 3;
+			while (i < 6) {
+				if (arrResp [i].GetComponent<SeleccionaRespuesta> ().clickState)
+					respDer= colDer [i - 3, 1];
+					i++;
+			}
+
+			if (respIzq == "si" && respDer == "si") {
+				print ("Bien");
+			} else {
+				print ("Mal");
+			}
+		}
+	}
+
+	private void ValidarColumnaIzquierda() {
+		for (int i = 0; i < 3; i++) {
+			if (arrResp [i].GetComponent<SeleccionaRespuesta> ().clickState)
+				contIzq++;
+		}
+
+		if (contIzq > 1 || contIzq < 1) {
+			panel.SetActive (true);
+		}
+	}
+
+	private void ValidarColumnaDerecha() {
+		for (int i = 3; i < 6; i++) {
+			if (arrResp [i].GetComponent<SeleccionaRespuesta> ().clickState)
+				contDer++;
+		}
+
+		if (contDer > 1 || contDer < 1) {
+			panel.SetActive (true);
+		}
+	}
+
+}
