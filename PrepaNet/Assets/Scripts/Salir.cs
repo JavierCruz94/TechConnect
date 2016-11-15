8eb4401a8051baf926a658ef2a9b8e1312685b54
JavierCruz94 <javiercruz_94@hotmail.com>
2016-11-15 01:39:42 -0600

    Videos X
    -

---
+++ PrepaNet/Assets/Scripts/Salir.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Salir : MonoBehaviour {
+
+	public GameObject panel;
+
+	public void MostrarPanel() {
+		panel.SetActive(true);
+	}
+
+	public void QuitarPanel() {
+		panel.SetActive (false);
+	}
+
+	public void SalirAplicacion() {
+		Application.Quit ();
+	}
+}
